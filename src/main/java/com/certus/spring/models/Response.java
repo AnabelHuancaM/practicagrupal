e7dba1c5411c4910fa325f159f01e27f93932cec
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-30 23:12:30 -0500

    response

--- src/main/java/com/certus/spring/models/Response.java
+++ src/main/java/com/certus/spring/models/Response.java
@@ -1,5 +1,29 @@
 package com.certus.spring.models;
 
-public class Response {
+import java.util.List;
 
+public class Response<T> {
+	private boolean estado;
+	private String mensaje;
+	private List<T> data;
+	
+	public boolean getEstado1() {
+		return estado;
+	}
+	public void setEstado1(boolean estado1) {
+		this.estado = estado1;
+	}
+	public String getMensaje1() {
+		return mensaje;
+	}
+	public void setMensaje1(String mensaje1) {
+		this.mensaje = mensaje1;
+	}
+	public List<T> getData1() {
+		return data;
+	}
+	public void setData1(List<T> data1) {
+		this.data = data1;
+	}
+	
 }
