287660839dfa8397a3601ea573cc6b73c73bde7b
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-30 19:42:24 -0500

    update

--- src/main/java/com/certus/spring/PracticagrupalApplication.java
+++ src/main/java/com/certus/spring/PracticagrupalApplication.java
@@ -2,7 +2,7 @@ package com.certus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//aaa
 @SpringBootApplication
 public class PracticagrupalApplication {
 
