7aaf616eb3ce418281dc0f294aa530f8e70b38ea
valentino17-0 <82229577+valentino17-0@users.noreply.github.com>
2022-11-30 19:49:17 -0500

    controller

--- src/main/java/com/certus/spring/controller/HomeController.java
+++ src/main/java/com/certus/spring/controller/HomeController.java
@@ -1,5 +1,72 @@
 package com.certus.spring.controller;
-//hola
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+
+
+
+
+@Controller
+@RequestMapping("/app")
 public class HomeController {
 
+	
+	
+   @GetMapping({ "/home", "/inicio", "/", "/Home", "/Inicio" })
+	public String Home(Model model) {
+
+
+		return "Home";
+
+	}
+   
+  
+   @GetMapping("/listar")
+	public String ListarPersonajes(Model model) {
+	   return "Lista";
+		}
+   
+   
+   
+   @GetMapping("/crear")
+	public String Formulario(Model model) {
+
+
+		return "Formulario";
+	}
+   
+   
+	@GetMapping("/Editar/{idPersonaje}")
+	public String EditarPersonaje(@PathVariable int idPersonaje, Model model) {
+
+
+
+		return "Formulario";
+	}
+	@GetMapping("/Elimnar/{idPersonaje}")
+	public String ElimnarPersonaje(@PathVariable int idPersonaje, Model model) {
+
+			return "errores";
+		}
+	
+
+	@PostMapping("/form")
+	public String creaPersonaje() {
+			
+			return "errores";
+		
+
+	}
 }
