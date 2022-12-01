a0c7f1c680c66e7cda0937d73b456ae76419dd03
AnabelHuancaM <91922240+AnabelHuancaM@users.noreply.github.com>
2022-11-30 23:08:07 -0500

    model usuario

--- src/main/java/com/certus/spring/models/Usuario.java
+++ src/main/java/com/certus/spring/models/Usuario.java
@@ -1,5 +1,34 @@
 package com.certus.spring.models;
 
 public class Usuario {
-//prueba
+	private String Nombre;
+	private String User;
+	private String Password;
+	private String Cod_Sucursal;
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getUser() {
+		return User;
+	}
+	public void setUser(String user) {
+		User = user;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getCod_Sucursal() {
+		return Cod_Sucursal;
+	}
+	public void setCod_Sucursal(String cod_Sucursal) {
+		Cod_Sucursal = cod_Sucursal;
+	}
+
 }
